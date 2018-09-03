3a05b3bd86352641aca7600dd4a457436c2783ff
ext.thomerson.tang <ext.thomerson.tang@ef.com>
2018-09-03 13:13:24 +0800

    update

---
+++ Thomerson.Algorithm/Thomerson.Algorithm/Collectionintersection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Thomerson.Algorithm
+{
+    #region question
+    //A、B两个整数集合，设计一个算法求他们的交集，尽可能的高效
+    #endregion
+
+    /// <summary>
+    /// 集合交集
+    /// </summary>
+    class Collectionintersection
+    {
+
+        public static void Test()
+        {
+
+        }
+
+        public static List<int> Intersection(List<int> arrA, List<int> arrB)
+        {
+            return new List<int>();
+        }
+    }
+}
